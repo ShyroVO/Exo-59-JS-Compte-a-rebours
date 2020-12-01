32ad03ecfd2badd32259763e9be368bb6154401b
ShyroVO <bosquet.laurie@outlook.com>
2020-12-01 11:59:00 +0100

    1.2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let seconde = 60;
 
 while (seconde >= 0) {
-    document.getElementById('compteur').innerHTML+= (seconde-1) +"<br>";
+    document.getElementById('compteur').innerHTML+= (seconde) +"<br>";
     seconde--;
 }
